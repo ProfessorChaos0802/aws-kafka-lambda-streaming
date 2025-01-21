f8866cf729533d13f1bb75c43f11e577785bc36b
ProfessorChaos0802 <1822rhit@pm.me>
2025-01-20 18:37:45 -0600

    Adding missing permissions to lambda

--- Terraform/roles.tf
+++ Terraform/roles.tf
@@ -44,3 +44,27 @@ resource "aws_iam_role_policy" "lambda_msk_publisher_policy" {
     ]
   })
 }
+
+resource "aws_iam_role_policy" "lambda_vpc_policy" {
+  role = aws_iam_role.s3_msk_image_publisher_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "ec2:CreateNetworkInterface",
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:DeleteNetworkInterface"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = "logs:*"
+        Resource = "*"
+      }
+    ]
+  })
+}
