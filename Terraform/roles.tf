5cfa5f293d3027aa2c4316b6df44479819fc46b5
ProfessorChaos0802 <1822rhit@pm.me>
2025-01-20 18:38:29 -0600

    Fixing format

--- Terraform/roles.tf
+++ Terraform/roles.tf
@@ -52,8 +52,8 @@ resource "aws_iam_role_policy" "lambda_vpc_policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Effect   = "Allow"
-        Action   = [
+        Effect = "Allow"
+        Action = [
           "ec2:CreateNetworkInterface",
           "ec2:DescribeNetworkInterfaces",
           "ec2:DeleteNetworkInterface"
