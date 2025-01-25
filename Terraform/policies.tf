0903fedf3f064868c0cbbdcd28e77a885075d568
ProfessorChaos0802 <1822rhit@pm.me>
2025-01-24 20:35:32 -0600

    Fixing

--- Terraform/policies.tf
+++ Terraform/policies.tf
@@ -15,7 +15,7 @@ resource "aws_msk_cluster_policy" "msk_lambda_streaming_cluster_policy" {
         "kafka:CreateVpcConnection",
         "kafka:GetBootstrapBrokers",
       ]
-      Resource = "arn:aws:kafka:us-east-2:048908104884:cluster/mskLambdaStreamingCluster/*"
+      Resource = aws_msk_cluster.msk_lambda_streaming_cluster.arn
     }]
   })
 }
