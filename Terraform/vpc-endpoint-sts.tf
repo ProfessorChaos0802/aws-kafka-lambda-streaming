639eefcad4d2853eec2ad76eeb7801b4f0f01016
ProfessorChaos0802 <1822rhit@pm.me>
2025-02-01 19:44:33 -0600

    more cxns

--- Terraform/vpc-endpoint-sts.tf
+++ Terraform/vpc-endpoint-sts.tf
@@ -3,11 +3,17 @@ resource "aws_vpc_endpoint" "sts" {
   service_name        = "com.amazonaws.${var.region}.sts"
   vpc_endpoint_type   = "Interface"
   private_dns_enabled = true
+
   subnet_ids = [
     aws_subnet.aws_msk_streaming_lambda_subnet.id
   ]
+
   security_group_ids = [
     aws_security_group.lambda_sg.id
+  ]
+
+  route_table_ids = [
+    aws_route_table.msk_lambda_streaming_route_table.id
   ]
 
   tags = {
