11a5da3cb13b526328b714cb7029512ac63eb057
ProfessorChaos0802 <1822rhit@pm.me>
2025-02-09 15:07:13 -0600

    Fixing error

--- Terraform/route-table.tf
+++ Terraform/route-table.tf
@@ -1,11 +1,6 @@
 resource "aws_route_table" "msk_lambda_streaming_route_table" {
   vpc_id = var.vpc_id
 
-  route {
-    cidr_block      = var.all_traffic
-    vpc_endpoint_id = aws_vpc_endpoint.sts.id
-  }
-
   tags = {
     Name    = "AWS MSK Lambda Streaming Route Table"
     project = var.project
