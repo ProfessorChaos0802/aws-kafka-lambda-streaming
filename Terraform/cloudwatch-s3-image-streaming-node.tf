ad8d101c47359b56abf48aad92dd0a947210bf67
ProfessorChaos0802 <1822rhit@pm.me>
2025-02-09 15:27:40 -0600

    removing node lambda

--- Terraform/cloudwatch-s3-image-streaming-node.tf
+++ Terraform/cloudwatch-s3-image-streaming-node.tf
@@ -1,15 +1,15 @@
-resource "aws_cloudwatch_log_group" "s3_msk_node_lambda_group" {
-  name              = "/aws/lambda/${aws_lambda_function.s3_image_publisher_node.function_name}"
-  retention_in_days = 3
+# resource "aws_cloudwatch_log_group" "s3_msk_node_lambda_group" {
+#   name              = "/aws/lambda/${aws_lambda_function.s3_image_publisher_node.function_name}"
+#   retention_in_days = 3
 
-  # Allow group deletion during terraform destroy
-  lifecycle {
-    prevent_destroy = false
-  }
+#   # Allow group deletion during terraform destroy
+#   lifecycle {
+#     prevent_destroy = false
+#   }
 
-  tags = {
-    Name    = "AWS MSK Node Lambda Streaming S3 MSK Log Group"
-    project = var.project
-    owner   = var.owner
-  }
-}
+#   tags = {
+#     Name    = "AWS MSK Node Lambda Streaming S3 MSK Log Group"
+#     project = var.project
+#     owner   = var.owner
+#   }
+# }
