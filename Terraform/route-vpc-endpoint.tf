115e74da8c525c92d6a86aa14a9d00f276d333b0
ProfessorChaos0802 <1822rhit@pm.me>
2025-02-09 14:32:27 -0600

    Different route table config

--- Terraform/route-vpc-endpoint.tf
+++ Terraform/route-vpc-endpoint.tf
@@ -1,7 +1,7 @@
-resource "aws_route" "sts_route" {
-  route_table_id         = aws_route_table.msk_lambda_streaming_route_table.id
-  destination_cidr_block = var.vpc_cidr
-  vpc_endpoint_id        = aws_vpc_endpoint.sts.id
+# resource "aws_route" "sts_route" {
+#   route_table_id         = aws_route_table.msk_lambda_streaming_route_table.id
+#   destination_cidr_block = var.vpc_cidr
+#   vpc_endpoint_id        = aws_vpc_endpoint.sts.id
 
-  depends_on = [aws_vpc_endpoint.sts]
-}
+#   depends_on = [aws_vpc_endpoint.sts]
+# }
